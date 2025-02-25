8ce11189002d4ff9f2b86e5af50c25529d75141d
aavi-sudo <aavinash@una.edu>
2025-02-25 11:50:06 -0600

    Delete calculate.ts

--- calculate.ts
+++
@@ -1,9 +0,0 @@
-function calculateTS(a: number, b: number, operation: "add" | "subtract" | "multiply"): number {
-  if (operation === "add") return a + b;
-  if (operation === "subtract") return a - b;
-  return a * b;
-}
-
-console.log(calculateTS(5, 3, "add")); //works fine 
-// console.log(calculateTS(5, "3", "add")); //compile time error
-
